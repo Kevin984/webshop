f194b7bf4b246fe7560ed601098d120231086fdc
Kevin <kevinbalk14@gmail.com>
2018-03-11 19:36:18 +0100

    build failed - fix?

--- src/main/java/ic/webshop/webservices/ProductServiceProvider.java
+++
@@ -1,9 +0,0 @@
-package ic.webshop.webservices;
-
-public class ProductServiceProvider {
-	private static ProductService voorraadService = new ProductService();
-
-	public static ProductService getProductService(){
-		return voorraadService;
-	}
-}
